4d25933b6b8adf3e64fa6f4943a879136a36558f
Lulu <lulusun18@gmail.com>
2023-05-27 14:49:02 -0400

    add all files

---
+++ .~c9_invoke_oiGrYe.py
@@ -0,0 +1,257 @@
+import os
+import datetime
+import requests
+
+#8WlLqUYMtkRokvOkYOsVT4pY7joahAZM
+
+# http://openlibrary.org/search.json?q=the+lord+of+the+rings
+# http://openlibrary.org/search.json?title=the+lord+of+the+rings
+# http://openlibrary.org/search.json?author=tolkien
+# http://openlibrary.org/search.json?q=the+lord+of+the+rings&page=2
+# http://openlibrary.org/search/authors.json?q=twain
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///books.db")
+
+
+@app.route("/", methods=["GET","POST"])
+@login_required
+def index():
+    """Show reading list"""
+
+    db.execute("CREATE TABLE IF NOT EXISTS list (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, user_id INTEGER NOT NULL, title TEXT NOT NULL, author TEXT NOT NULL, read BOOLEAN DEFAULT 0 NOT NULL, add_time TEXT NOT NULL, read_time TEXT DEFAULT 'unread', diff TEXT DEFAULT 'none')")
+
+    books_list = db.execute("SELECT title, author FROM list WHERE user_id = ? AND read=0", session["user_id"])
+
+    if request.method == "GET":
+        return render_template("index.html", books_list = books_list)
+
+    if request.method == "POST":
+        books_read = request.form.getlist("book")
+        time = datetime.date.today()
+
+        for book in books_read:
+            book = book.strip()
+
+            added = db.execute("SELECT add_time FROM list WHERE user_id = ? AND title = ?", session["user_id"], book)[0]['add_time']
+            d = (time - datetime.datetime.strptime(added, '%Y-%m-%d').date()).days
+
+            db.execute("UPDATE list SET read = 1, read_time = ?, diff = ? WHERE user_id = ? AND title = ?", time, d, session["user_id"], book)
+
+        books_list = db.execute("SELECT title, author FROM list WHERE user_id = ? AND read=0", session["user_id"])
+
+        return render_template("index.html", books_list = books_list)
+
+
+@app.route("/add", methods=["GET", "POST"])
+@login_required
+def add():
+    """Add books to list"""
+
+    if request.method == "GET":
+        return render_template("add.html")
+
+    if request.method == "POST":
+        if not request.form.get("title"):
+            return apology("input title")
+
+        if not request.form.get("author"):
+            return apology("input author")
+
+        title = request.form.get("title").title()
+        author = request.form.get("author").title()
+        time = datetime.date.today()
+
+        search_title = title.replace(" ", "+")
+        # print(search_title)
+        # book_info = requests.get(f"http://openlibrary.org/search.json?title={search_title}").json()
+        # authors = []
+        # for book in book_info['docs']:
+        #     print(book['title'])
+        # print(authors)
+        # return render_template("add.html", book_info = book_info)
+        # print(book_info.json()['docs'][0]['author_name'])
+
+        db.execute("INSERT INTO list (user_id, title, author, add_time) VALUES (?, ?, ?, ?)", session["user_id"],title, author, time)
+        return redirect("/")
+
+
+@app.route("/read", methods=["GET", "POST"])
+@login_required
+def read():
+    """Show list of books read"""
+
+    if request.method == "GET":
+        books_list = db.execute("SELECT title, author, read_time, add_time FROM list WHERE user_id = ? AND read=1", session["user_id"])
+        return render_template("read.html", books_list = books_list)
+
+    if request.method == "POST":
+        books_unread = request.form.getlist("book")
+
+        for book in books_unread:
+            book = book.strip()
+            db.execute("UPDATE list SET read = 0 WHERE user_id = ? AND title = ?", session["user_id"], book)
+
+        books_list = db.execute("SELECT title, author, read_time, add_time FROM list WHERE user_id = ? AND read=1", session["user_id"])
+        # [{'title': 'Jane Eyre', 'author': 'Charlotte Bronte', 'read_time': '2021-12-26'}]
+        # d = datetime.datetime.strptime(s, '%m/%d/%Y')
+
+        return render_template("read.html", books_list = books_list)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # REGISTER USER
+
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username is unique
+        if len(rows) > 0:
+            return apology("username already taken", 400)
+
+        pwd = request.form.get("password")
+
+        # Ensure passwords match
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("passwords must match", 400)
+
+        # Make hash for password
+        pwd = generate_password_hash(request.form.get("password"))
+
+        # Add information into SQL
+        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", request.form.get("username"), pwd)
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
+
+
+@app.route("/recommend", methods=["GET", "POST"])
+@login_required
+def recommend():
+    recommends = requests.get(f"https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?&api-key=8WlLqUYMtkRokvOkYOsVT4pY7joahAZM").json()
+
+    db.execute("CREATE TABLE IF NOT EXISTS hardcover_fiction (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, title TEXT NOT NULL, author TEXT NOT NULL, book_image TEXT NOT NULL)")
+
+    # db.execute("INSERT INTO table (id, name, age) VALUES(1, "A", 19) ON DUPLICATE KEY UPDATE name="A", age=19")
+    recommend_list = {}
+
+    # 'title': None, 'author': None, 'description': None, 'rank': None, 'book_image': None
+    for entry in recommends["results"]["books"]:
+        db.execute("INSERT INTO hardcover_fiction (title, author, book_image) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE title=?, author=?, book_image=?",
+        entry['title'], entry['author'], entry['description'],
+        entry['title'], entry['author'], entry['description'])
+        recommend_list[entry['title'].title()] = {'author':entry['author'], 'description':entry['description'], 'rank':entry['rank'], 'book_image':entry['book_image']}
+
+    if request.method == "GET":
+        return render_template("recommend.html", recs=recommend_list)
+
+    if request.method == "POST":
+        books_unread = request.form.getlist("book")
+
+        for book in books_unread:
+            book = book.strip()
+            db.execute("UPDATE list SET read = 0 WHERE user_id = ? AND title = ?", session["user_id"], book)
+
+        books_list = db.execute("SELECT title, author, read_time, add_time FROM list WHERE user_id = ? AND read=1", session["user_id"])
+        # [{'title': 'Jane Eyre', 'author': 'Charlotte Bronte', 'read_time': '2021-12-26'}]
+        # d = datetime.datetime.strptime(s, '%m/%d/%Y')
+
+        return redirect("/")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
---
+++ README.md
@@ -0,0 +1,24 @@
+# READINGLIST
+#### Video Demo:  [YOUTUBE](https://youtu.be/6F37pL4LeHM)
+#### Description:
+##### Overview:
+This is a web app that lets you track books that you want to read. It includes a login feature for unique users. The app will track when you added the book to your reading list and to your completed books list. There is also a feature that suggests books from the New York Times hardcover fiction bestsellers list, from which you can also add to your reading list.
+
+##### Design:
+This app is meant to be a simple book list tracker. I chose not to use an API to check each user's books against some library database, simply because there are too many books to search through, and the API could either not be expansive enough to cover each book, since there are books that have multiple editions, or with the same titles. Because I do not have enough time to carefully craft something that could properly check a user's input for validity, I leave it up to the user to know which book they are trying to input.
+
+For the New York Times recommendations, I kept it simple by using just the hardcover fiction bestsellers list. There are certainly many other lists, but I would not have had enough time to put them all in. If I were to continue working on this project, I would have a selection on the web app to allow a user to decide which list they want to see, and allow them to add whichever book to their reading list.
+
+I designed the app to be simple and straightforward so that it can be quick to navigate through and easy to manage.
+
+I did not include a way for the user to delete or edit their entry, but if I were to have the time, I would have wanted to include this as an additional feature.
+
+##### Application.py:
+This file is the main script for this project. It contains the features for each of the pages. There is a global variable that will access the New York Times Books API to collect data from their most recent hardcover fiction bestsellers list. The register, login, and logout functions are based on the functions from the latest problem set. Book list data is stored in the SQL database called "books", and is called for loading the lists on the Index, Read, and Recommends pages. The Add page allows the user to fill out a form of the title and author to add a book to their reading list. There is a Time variable that records the date the user adds books to each respective list.
+
+##### books.db:
+This is a SQLite3 database that contains all the data for each user's book lists.
+
+##### helpers.py:
+This is a file of helper functions, based on the functions from the latest problem set.
+
